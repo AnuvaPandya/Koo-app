68cccce726df0c4bc271b3b2bd66a26401ba5e99
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-10-18 13:52:57 +0530

    wrapped in a Paper component

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -2,20 +2,23 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
+import { Paper } from "@mui/material";
 
 export default function PostCard({ title, body }) {
   return (
     <div style={styles.containerStyle}>
-      <Card raised={true} variant="outlined" sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            {body}
-          </Typography>
-        </CardContent>
-      </Card>
+      <Paper>
+        <Card raised={true} variant="outlined" sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {body}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Paper>
     </div>
   );
 }
