3c78bc33ab578a1971d184a26ec77c8b3992ad19
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-10-18 13:33:02 +0530

    Completed basic of implementation of the Koo-app

--- src/FetchPosts.js
+++ src/FetchPosts.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import PostCard from './components/PostCard';
-import { POSTS_URL } from './constants/globalConstants';
-
+import { grey } from "@mui/material/colors";
+import React from "react";
+import { useState, useEffect } from "react";
+import PostCard from "./components/PostCard";
+import { POSTS_URL } from "./constants/globalConstants";
 
 function FetchPosts() {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        fetch(POSTS_URL)
-        .then(response => response.json())
-        .then(result => setPosts(result.data))
-    }, [])
+  useEffect(() => {
+    fetch(POSTS_URL)
+      .then((response) => response.json())
+      .then((result) => setPosts(result.data));
+  }, []);
 
-    useEffect(() => {
-       console.log(posts);
-    }, [posts])
+  useEffect(() => {
+    console.log(posts);
+  }, [posts]);
 
-    
-    return(
-        <div>
-            {posts.map(post => {
-                return (
-                    <div>
-                        <PostCard title={post.title} body={post.body} />
-                    </div>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div>
+      {posts.map((post) => {
+        return (
+          <div style={{ marginTop: "20px", marginBottom: "20px" }}>
+            <PostCard title={post.title} body={post.body} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default FetchPosts;
